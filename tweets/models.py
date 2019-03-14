99a26ddc01e4e1dd20c2d8c80e7583562c4ddbac
Abhie <abhie.lp@gmail.com>
2019-03-15 00:23:25 +0530

    Added Tweet model

--- tweets/models.py
+++ tweets/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tweet(models.Model):
+    content = models.CharField(max_length=140)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = "-created_on", "content", "user",
+
+    def __str__(self):
+        return self.content
