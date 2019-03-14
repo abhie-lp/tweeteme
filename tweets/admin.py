42875727280de73a1693d4fab59a46308c3fe5b9
Abhie <abhie.lp@gmail.com>
2019-03-15 00:23:56 +0530

    Registered Tweet model

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,3 +1,9 @@
+from .models import Tweet
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(Tweet)
+class TweetAdmin(admin.ModelAdmin):
+    list_display = "id", "content", "user", "created_on", "updated_on",
+    list_display_links = "content",
+    search_fields = "user",
