bae1c3cef1bc2e0bbff4b6d1205b3f859e45ce13
Abhie <abhie.lp@gmail.com>
2019-03-15 22:19:33 +0530

    Added TweetForm in tweet_list context

--- tweets/views.py
+++ tweets/views.py
@@ -1,5 +1,7 @@
+from . import forms
 from django.shortcuts import render
 
 
 def tweet_list(request):
-    return render(request, "tweets/tweet_list.html")
+    tweet_form = forms.TweetForm()
+    return render(request, "tweets/tweet_list.html", context={"tweet_form": tweet_form})
