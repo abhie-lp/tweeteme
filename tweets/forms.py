3699fa58d1544606e3ee299c0fa2f6122514832a
Abhie <abhie.lp@gmail.com>
2019-03-15 22:19:08 +0530

    Added form for Tweet model

---
+++ tweets/forms.py
@@ -0,0 +1,15 @@
+from .models import Tweet
+from django import forms
+
+
+class TweetForm(forms.ModelForm):
+
+    class Meta:
+        model = Tweet
+        fields = "content",
+        labels = {"content": ""}
+        widgets = {"content": forms.Textarea(attrs={
+            "placeholder": "Your tweet",
+            "class": "form-control form-group",
+            "rows": 5,
+        })}
