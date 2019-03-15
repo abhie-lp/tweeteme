0da5db51e1bbfdc5b5f2687b09b49c4dc1961056
Abhie <abhie.lp@gmail.com>
2019-03-15 22:21:16 +0530

    overrided perform_create of TweetViewSet

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -7,3 +7,6 @@ from rest_framework.viewsets import ModelViewSet
 class TweetViewSet(ModelViewSet):
     serializer_class = TweetSerializer
     queryset = Tweet.objects.all()
+
+    def perform_create(self, serialiazer):
+        return serialiazer.save(user=self.request.user)
