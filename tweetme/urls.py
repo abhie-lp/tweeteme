71430e3178cd4884f14b7ed3bdf9c77a80d833e2
Abhie <abhie.lp@gmail.com>
2019-03-15 21:36:06 +0530

    Added url tweet-api

--- tweetme/urls.py
+++ tweetme/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("tweet/", include("tweets.urls", namespace="tweet")),
+    path("tweet-api/", include("tweets.api.urls", namespace="tweet_api")),
 ]
