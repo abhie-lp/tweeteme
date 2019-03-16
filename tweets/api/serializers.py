fa97bc4946abb5524a4c1eaf24aa07db70791614
Abhie <abhie.lp@gmail.com>
2019-03-16 20:32:09 +0530

    Added serializer for Django User model

--- tweets/api/serializers.py
+++ tweets/api/serializers.py
@@ -1,10 +1,18 @@
 from ..models import Tweet
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = "id", "username", "get_full_name",
+
+
 class TweetSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
 
     class Meta:
         model = Tweet
         fields = "id", "content", "user", "created_on", "updated_on",
-        read_only_fields = "user",
