d38d8b7014fcefd20893fa3c19a276ebc0207186
Abhie <abhie.lp@gmail.com>
2019-03-18 00:26:52 +0530

    Added accounts app

--- tweetme/settings.py
+++ tweetme/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "tweets.apps.TweetsConfig",
+    "accounts.apps.AccountsConfig",
     "rest_framework"
 ]
 
