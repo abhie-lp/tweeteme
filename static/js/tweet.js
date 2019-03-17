40905965c9ed210c08a61cf888a8a8be8eab6e69
Abhie <abhie.lp@gmail.com>
2019-03-17 13:31:55 +0530

    AJAX call to handle the delete request

--- static/js/tweet.js
+++ static/js/tweet.js
@@ -15,10 +15,9 @@ function loadContent(content_div, get_url) {
                           <div class="media-body">
                            <a class="text-dark font-weight-bold" href="#user">${contentUser.get_full_name}</a> <span 
                            class="text-muted"> 
-                            @${contentUser.username} &middot; 
-${time}</span>
+                            @${contentUser.username} &middot; ${time}</span>
                           <p class="tweet-content" data-id="${contentId}">${content}</p>
-                          <a class="tweet-detail-link" href="#">View</a> |
+                          <a class="tweet-detail-link" href="#">View</a> | <a class="tweet-delete-link float-right text-danger" href="">Delete</a>
                           </div>
                         </div><hr>
                         `;
@@ -27,7 +26,7 @@ ${time}</span>
             } else {
                 $(content_div).append(contentHTML);
             }
-        })
+        });
     }
 
 
@@ -90,10 +89,50 @@ ${time}</span>
 <small class="text-muted font-weight-light">@${contentUser.username}</small><br><span class="font-weight-bold">
 ${content}</span><br><small class="text-muted">${time}</small>`);
 
-                $(".modal").modal();
+                $(".modal-detail").modal();
             },
             error: function(err) {
                 console.log("Err in single tweet");
+                console.log(err);
+            }
+        })
+    });
+
+
+    // Handle the tweet delete click
+    $(document.body).on("click", "a.tweet-delete-link", function(event) {
+        event.preventDefault();
+        console.log("Delete the tweet");
+        const this_ = $(this);
+        contentID = this_.parent().children("p.tweet-content").attr("data-id");
+        const thisContent = this_.parent().find(".tweet-content").text();
+        const deleteModal = $(".modal-delete");
+        deleteModal.find("h5.modal-title").html(`<span class="text-danger font-weight-bold">Delete:</span> <span class="font-weight-bold">${thisContent}</span>`);
+        deleteModal.modal();
+        console.log("id to delete: ", contentID);
+        contentArea = [this_.parent().parent(), this_.parent().parent().next()];
+    });
+
+
+    // Handle the modal-delete submission
+    $(".modal-delete form").submit(function(event) {
+        event.preventDefault();
+        console.log("confirm delete tweet");
+        const csrf = $(this).children("input").attr("value");
+
+        $.ajax(apiURL + contentID, {
+            method: "DELETE",
+            headers: {"X-CSRFToken": csrf},
+            content: "application/json",
+            success: function(data) {
+                console.log("Deletion successfull");
+                $(".modal-delete").modal("hide");
+                contentArea.forEach(function(ele) {
+                    ele.remove();
+                });
+            },
+            error: function(err) {
+                console.log("Err in deletion");
                 console.log(err);
             }
         })
