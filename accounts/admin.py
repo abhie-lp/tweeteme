0556049a581b180b624b2e01ac8b0b123078786e
Abhie <abhie.lp@gmail.com>
2019-03-19 13:23:15 +0530

    Registered in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
+from .models import UserProfile
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = "id", "user", "profile_pic", "profile_thumb",
+    list_display_links = "user",
+    search_fields = "user",
+    readonly_fields = "profile_thumb", "following",
