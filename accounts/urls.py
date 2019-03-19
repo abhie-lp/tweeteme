99df62d95f163ae5539a682303718d06c72a55bd
Abhie <abhie.lp@gmail.com>
2019-03-19 16:23:22 +0530

    Added RegisterView, LoginView and LogoutView

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+
+app_name = "user"
+
+urlpatterns = [
+    path("register/", views.RegisterView.as_view(), name="register"),
+    path("login/", LoginView.as_view(template_name="accounts/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+]
