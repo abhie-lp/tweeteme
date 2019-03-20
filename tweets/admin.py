c5555a4e3c3ff250ce271d0dc95cb25a79790c28
Abhie <abhie.lp@gmail.com>
2019-03-21 00:02:19 +0530

    Registered Post and changed other model values

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,9 +1,33 @@
-from .models import Tweet
+from . import models
 from django.contrib import admin
 
 
-@admin.register(Tweet)
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+
+    def tweet_id(self, inst):
+        return inst.tweet.id
+
+    def retweet_id(self, inst):
+        return inst.retweet.id
+
+    list_display = "id", "user", "created_on", "tweet_id", "retweet_id",
+    list_display_links = "user",
+
+
+@admin.register(models.Tweet)
 class TweetAdmin(admin.ModelAdmin):
     list_display = "id", "content", "user", "created_on", "updated_on",
     list_display_links = "content",
     search_fields = "user",
+
+
+@admin.register(models.Retweet)
+class RetweetAdmin(admin.ModelAdmin):
+
+    def parent_tweet_id(self, inst):
+        return f"{inst.parent_tweet.pk} - {inst.parent_tweet.content[:20]}"
+
+    list_display = "id", "parent_tweet_id", "user", "created_on",
+    list_display_links = "id", "parent_tweet_id",
+    raw_id_fields = "parent_tweet",
