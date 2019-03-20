cc27ffdf0e4b56f1ab5c54149ccef08202c447a6
Abhie <abhie.lp@gmail.com>
2019-03-21 00:00:56 +0530

    Redesigned the models, added Post model and inheritted in Tweet and Retweet model

--- tweets/models.py
+++ tweets/models.py
@@ -3,17 +3,25 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 
-class Tweet(models.Model):
-    content = models.CharField(max_length=140)
+class Post(models.Model):
+    """Base model for user post"""
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = "-created_on", "content", "user",
+        ordering = "-created_on", "user",
+
+
+class Tweet(Post):
+    content = models.CharField(max_length=140)
+    updated_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.content
 
-    def get_absolute_url(self):
-        return reverse("tweet:detail", args=[self.id])
+
+class Retweet(Post):
+    parent_tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE, related_name="retweet")
+
+    def __str__(self):
+        return self.parent_tweet.content
