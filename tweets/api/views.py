a2552b0986e0d33226fe873bce2009a909bcfbd2
Abhie <abhie.lp@gmail.com>
2019-03-20 13:43:35 +0530

    Added search filter to TweetModelViewSet

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -2,6 +2,7 @@ from .pagination import DefaultPagination
 from .serializers import TweetSerializer
 from ..models import Tweet
 
+from rest_framework import filters
 from rest_framework.viewsets import ModelViewSet
 
 
@@ -9,6 +10,8 @@ class TweetViewSet(ModelViewSet):
     serializer_class = TweetSerializer
     queryset = Tweet.objects.all()
     pagination_class = DefaultPagination
+    filter_backends = filters.SearchFilter,
+    search_fields = "content", "user__username", "user__first_name", "user__last_name",
 
     def perform_create(self, serialiazer):
         return serialiazer.save(user=self.request.user)
