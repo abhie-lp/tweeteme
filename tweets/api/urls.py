9eb405e1914ef3e6d27870bd474423dc6ec270f5
Abhie <abhie.lp@gmail.com>
2019-03-22 10:49:52 +0530

    Removed PostListViewSet from router and added PostListAPIView to urlpatterns

--- tweets/api/urls.py
+++ tweets/api/urls.py
@@ -5,10 +5,10 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register("tweet", views.TweetViewSet)
 router.register("retweet", views.RetweetViewSet)
-router.register("posts", views.PostListViewSet)
 
 app_name = "tweet_api"
 
 urlpatterns = [
-    path("", include(router.urls)),
+    path("model/", include(router.urls)),
+    path("", views.PostListAPIView.as_view(), name="posts_list"),
 ]
