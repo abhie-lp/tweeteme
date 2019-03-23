68307a7aa40bb653ca164f80fb476650ad39708f
Abhie <abhie.lp@gmail.com>
2019-03-23 21:33:44 +0530

    Changed to success url to login

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,9 @@
 from . import forms
 from django.views.generic.edit import CreateView
-from django.contrib.auth.models import User
+from django.urls import reverse_lazy
 
 
 class RegisterView(CreateView):
     form_class = forms.RegisterForm
-    success_url = "/tweet/"
+    success_url = reverse_lazy("user:login")
     template_name = "accounts/register.html"
