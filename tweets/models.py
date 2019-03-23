70fc6f0a0ebfe4193961b4b8f11f30b311dc2b12
Abhie <abhie.lp@gmail.com>
2019-03-23 12:27:22 +0530

    Added RetweetManager to return whether tweet was retweeted today

--- tweets/models.py
+++ tweets/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.utils import timezone
 from django.contrib.auth.models import User
 
 
@@ -20,8 +21,26 @@ class Tweet(Post):
         return self.content
 
 
+class RetweetManager(models.Manager):
+
+    def retweeted_today(self, user, parent_tweet):
+        print("Entered manager")
+        current_date = timezone.now()
+        qs = self.get_queryset().filter(user=user,
+                                        parent_tweet=parent_tweet,
+                                        created_on__year=current_date.year,
+                                        created_on__month=current_date.month,
+                                        created_on__day=current_date.day)
+        if qs.exists():
+            print("Returning None")
+            return True
+        else:
+            return False
+        
+
 class Retweet(Post):
     parent_tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE, related_name="retweet")
+    objects = RetweetManager()
 
     def __str__(self):
         return self.parent_tweet.content
