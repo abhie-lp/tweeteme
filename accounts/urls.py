baf4bf276d243aced47199bb1cfbe27d87bdfe62
Abhie <abhie.lp@gmail.com>
2019-04-01 23:09:13 +0530

    View to handle follow request and current status of logged in user following

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 app_name = "user"
 
 urlpatterns = [
+    path("<str:user>/follow/", views.follow_user, name="follow_user"),
     path("register/", views.RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(template_name="accounts/login.html"), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
-from . import forms
+from . import forms, models
 
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.http.response import JsonResponse
 from django.contrib.auth.models import User
 from django.views.generic.edit import CreateView
 
@@ -14,4 +15,11 @@ class RegisterView(CreateView):
 
 def user_posts(request, username):
     user = User.objects.get(username=username)
-    return render(request, "accounts/user_posts.html", {"user": user})
+    follow_text = models.UserProfile.objects.is_following(user, request.user)
+    return render(request, "accounts/user_posts.html", {"user": user, "follow_text": follow_text})
+
+
+def follow_user(request, user):
+    user_obj = User.objects.get(username=user)
+    follow_status = models.UserProfile.objects.toggle_following(user_obj, request.user)
+    return JsonResponse({"follow_status": follow_status})
