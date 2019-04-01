461afa1a1d2f9ce239a04f72b9dcbaf21ca9c34c
Abhie <abhie.lp@gmail.com>
2019-04-01 19:59:51 +0530

    Added user_posts view to list all the posts of a user

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("register/", views.RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(template_name="accounts/login.html"), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("<str:username>/", views.user_posts, name="user_posts"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,17 @@
 from . import forms
-from django.views.generic.edit import CreateView
+
+from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.contrib.auth.models import User
+from django.views.generic.edit import CreateView
 
 
 class RegisterView(CreateView):
     form_class = forms.RegisterForm
     success_url = reverse_lazy("user:login")
     template_name = "accounts/register.html"
+
+
+def user_posts(request, username):
+    user = User.objects.get(username=username)
+    return render(request, "accounts/user_posts.html", {"user": user})
