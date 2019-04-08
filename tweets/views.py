8b44c450dee62794bfa4f11e949c40859e1f3233
Abhie <abhie.lp@gmail.com>
2019-04-09 00:54:57 +0530

    Added user in search results

--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,7 @@
 from . import forms
 
 from django.shortcuts import render
+from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
@@ -10,6 +11,9 @@ def tweet_list(request):
     tweet_form = forms.TweetForm()
     logged_user = request.user
     logged_profile = logged_user.userprofile
+
+    context = {"tweet_form": tweet_form}
+
     following = list(logged_profile.following.values_list("id", flat=True))
     following.append(logged_user.id)
     recommended_users = User.objects.exclude(
@@ -19,6 +23,38 @@ def tweet_list(request):
                                                      "first_name",
                                                      "last_name",
                                                      "userprofile__profile_thumb").order_by("?")[:6]
-    return render(request, "tweets/tweet_list.html", context={
-                                                            "tweet_form": tweet_form,
-                                                            "recommended_users": recommended_users})
+
+    context["recommended_users"] = recommended_users
+
+    search = request.GET.get("search")
+
+    if search:
+        search_results = User.objects.filter(
+            Q(username__icontains=search) |
+            Q(first_name__icontains=search) |
+            Q(last_name__icontains=search)).values(
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "userprofile__profile_thumb",
+        )
+
+        search_following = []
+        not_following = []
+
+        for user in search_results:
+            user_id = user["id"]
+            if user_id in following:
+                search_following.append(user)
+            else:
+                not_following.append(user)
+
+        context["search"] = True
+        context["search_following"] = search_following
+        context["not_following"] = not_following
+
+        if not search_results:
+            context["no_result"] = True
+
+    return render(request, "tweets/tweet_list.html", context=context)
