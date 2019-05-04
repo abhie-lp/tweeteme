c6d75d20d136b4a53475791e0db72f43af537e99
Abhishek Tiwari <abhie.lp@gmail.com>
2019-05-04 21:30:16 +0530

    Added api and view urls to root url

--- tweetme/urls.py
+++ tweetme/urls.py
@@ -7,8 +7,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path("api/tag/", include("tags.api.urls", namespace="tag_api")),
     path('admin/', admin.site.urls),
     path("tweet/", include("tweets.urls", namespace="tweet")),
+    path("tag/", include("tags.urls", namespace="tag")),
     path("myaccount/", acc_view.profile_update_view, name="myprofile"),
     path("api/", include("tweets.api.urls", namespace="tweet_api")),
     path("user/", include("accounts.urls", namespace="user")),
